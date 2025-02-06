6b39e9febb78c0d8ff6c28e509a2d428fbdf428f
rafiistcool <tillrafael@icloud.com>
2025-02-06 20:37:51 +0100

    update left and right arrow

--- brainPop/Frontend/src/components/LearningMode.vue
+++ brainPop/Frontend/src/components/LearningMode.vue
@@ -13,38 +13,48 @@ const configureContentOfFlashcard = () => {
 }
 </script>
 
-<template>
-<body>
+<<template>
+  <div class="learning-mode-window">
+    <h1 id="learnMode-h1">Learn-Mode</h1>
+    <div class="learning-mode-window-content">
 
-<div class="learning-mode-window">
-  <h1 id="learnMode-h1">Learn-Mode</h1>
-  <div class="learning-mode-window-content">
-    <div class="left-learning-mode-window">
-      <img src="../assets/icons/arrowIcon.png" alt="arrow left" id="leftArrow">
-    </div>
-
-
-    <div class="flashcard-container" @click="toggleVisible">
-      <div class="flashcard" :class="{ flipped: visible }">
-        <div class="flashcardFront" v-if="visible">
-
-          <p class="userContent">Hier sollte später Text stehen</p>
-        </div>
-        <div class="flashcardBack" v-else>
-          <p class="userContent">Hier sollte der zweite Text stehen</p>
+      <!-- Left Arrow -->
+      <div class="arrow-container">
+        <div class="arrow arrow-left">
+          <svg viewBox="0 0 24 24" class="arrow-svg">
+            <path d="M15 18l-6-6 6-6" />
+          </svg>
         </div>
       </div>
-    </div>
-    <div class="right-learning-mode-window">
-      <img src="../assets/icons/arrowIcon.png" alt="arrow right" id="rightArrow"/>
-    </div>
 
+      <!-- Flashcard -->
+      <div class="flashcard-container" @click="toggleVisible">
+        <div class="flashcard">
+          <div class="flashcardFront" v-if="visible">
+            <p class="userContent">Hier sollte später Text stehen</p>
+          </div>
+          <div class="flashcardBack" v-else>
+            <p class="userContent">Hier sollte der zweite Text stehen</p>
+          </div>
+        </div>
+      </div>
+
+      <!-- Right Arrow -->
+      <div class="arrow-container">
+        <div class="arrow arrow-right">
+          <svg viewBox="0 0 24 24" class="arrow-svg">
+            <path d="M9 18l6-6-6-6" />
+          </svg>
+        </div>
+      </div>
+
+    </div>
+    <div class="configure">
+      <button class="baseButtonLayout" @click="configureContentOfFlashcard">
+        Configure Content
+      </button>
+    </div>
   </div>
-  <div class="configure">
-    <button class="baseButtonLayout" @click="configureContentOfFlashcard">Configure Content</button>
-  </div>
-</div>
-</body>
 </template>
 
 <style>
