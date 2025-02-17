60c0bed67ac15c57d8d51813c09d99489bb5c53f
SebastianRieger <basti_rieger@hotmail.com>
2025-02-17 09:26:44 +0100

    fixed label placing

--- brainPop/Frontend/src/components/LoginPage.vue
+++ brainPop/Frontend/src/components/LoginPage.vue
@@ -9,13 +9,14 @@
     <form>
       <div class="input-group">
         <label for="email">E-Mail</label>
-        <input type="text" id="username" name="username" required>
+        <input type="text" id="email" name="email" required>
       </div>
       <div class="input-group">
         <label for="password">Password</label>
         <input type="password" id="password" name="password" required>
       </div>
       <button type="submit" class="login-button">Login</button>
+      <button type="button" class="register-button">Register</button>
     </form>
     <a href="#" style="color: #6fB98F" class="forgot-password">Forgot Password?</a>
   </div>
