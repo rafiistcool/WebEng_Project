c756bd866e33ce59a7168be2eceaf9016944dabe
Rafael Till <tillrafael@icloud.com>
2025-03-06 09:27:52 +0100

    add basic ts functions

--- brainPop/Frontend/src/components/CardCreation.vue
+++ brainPop/Frontend/src/components/CardCreation.vue
@@ -1,5 +1,31 @@
 <script setup lang="ts">
 
+class Card {
+  id: number;
+  question: string;
+  answer: string;
+  category: string;
+
+  constructor(id: number, question: string, answer: string, category: string) {
+    this.id = id;
+    this.question = question;
+    this.answer = answer;
+    this.category = category;
+  }
+}
+
+const cards: Card[] = [];
+
+const startLearningmode = () => {
+
+}
+
+const addCard = () => {
+
+}
+const edit = () => {
+
+}
 </script>
 
 <template>
@@ -7,15 +33,18 @@
   <h1>Card Creation</h1>
   <div class="content-wrapper">
     <div class="card-creation-buttons">
-      <button class="card-creation-button">Starten</button>
-      <button class="card-creation-button">Hinzufügen</button>
-      <button class="card-creation-button">Bearbeiten</button>
+      <button class="card-creation-button" @click="startLearningmode">Starten</button>
+      <button class="card-creation-button" @click="addCard">Hinzufügen</button>
+      <button class="card-creation-button" @click="edit">Bearbeiten</button>
     </div>
     <div class="card-contents">
       <p>Test Inhalt</p>
     </div>
   </div>
 
+  <div class="card-creation-popup" >
+
+  </div>
 </div>
 </template>
 
