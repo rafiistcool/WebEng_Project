9e27278d21b29fa5160256f2ca58399a2ed6e18c
SebastianRieger <basti_rieger@hotmail.com>
2025-03-12 19:08:36 +0100

    sidebar, login register basic functions

--- brainPop/Frontend/src/components/RegisterPage.vue
+++ brainPop/Frontend/src/components/RegisterPage.vue
@@ -1,28 +1,50 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
 
+const email = ref('');
+const password = ref('');
+const passwordConfirm = ref('');
+const router = useRouter();
+
+const register = () => {
+  if (email.value && (password.value === passwordConfirm.value)) {
+    if (password.value === passwordConfirm.value) {
+      localStorage.setItem('userEmail', email.value);
+      localStorage.setItem('userPassword', password.value);
+      alert('Registrierung erfolgreich!');
+      router.push('/login');
+    }else{
+      alert('Bitte geben Sie zweimal das selbe Passwort ein!')
+    }
+  } else {
+    alert('Bitte alle Felder ausfüllen!');
+  }
+};
 </script>
 
 <template>
   <body>
   <div class="login-container">
     <h2>Registrieren</h2>
-    <form>
+    <form @submit.prevent="register">
       <div class="input-group">
         <label for="email">E-Mail</label>
-        <input type="text" id="email" name="email" required>
+        <input type="text" id="email" v-model="email" required>
       </div>
       <div class="input-group">
         <label for="password">Passwort</label>
-        <input type="password" id="password" name="password" required>
+        <input type="password" id="password" v-model="password" required>
       </div>
       <div class="input-group">
-        <label for="password">Passwort wiederholen</label>
-        <input type="password" id="password" name="password" required>
+        <label for="passwordConfirm">Passwort wiederholen</label>
+        <input type="password" id="passwordConfirm" v-model="passwordConfirm" required>
       </div>
       <button type="submit" class="register-button">Registrieren</button>
     </form>
   </div>
   </body>
+
 </template>
 
 <style scoped>
--- brainPop/Frontend/src/components/SideBar.vue
+++ brainPop/Frontend/src/components/SideBar.vue
@@ -19,15 +19,13 @@ const closeSidebar = () => {
       <div class="sidebar-content">
         <h2>Menu</h2>
         <ul>
-
-          <li><router-link to="/">Home</router-link></li>
-          <li><router-link to="/Card">Card</router-link></li>
           <li><router-link to="/startscreen">Startscreen</router-link></li>
-          <li><router-link to="/login">login</router-link></li>
-          <li><router-link to="/register">register</router-link></li>
-          <li><router-link to="/explorer">Explorer</router-link></li>         
+          <li><router-link to="/login">Login</router-link></li>
+          <li><router-link to="/register">Register</router-link></li>
+          <li><router-link to="/explorer">Explorer</router-link></li>
           <li><router-link to="/cardcreation">Card Creation</router-link></li>
-
+          <li><router-link to="/Card">Learning Mode</router-link></li>
+    
         </ul>
       </div>
     </div>
