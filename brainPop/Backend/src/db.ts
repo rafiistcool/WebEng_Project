cf0a11d61465903ef2e6a6d3ece937b31c07a6c2
rafiistcool <tillrafael@icloud.com>
2025-07-06 12:59:00 +0200

    refactor: use dotenv for database configuration and update connection structure

--- brainPop/Backend/src/db.ts
+++ brainPop/Backend/src/db.ts
@@ -1,6 +1,15 @@
 import pgPromise from "pg-promise";
+import "dotenv/config";
+
+const cn = {
+    host: process.env.DB_HOST,
+    port:parseInt(process.env.DB_PORT || "5432",10),
+    database: process.env.DB_DATABASE,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+};
 
 const pgp = pgPromise();
-const db = pgp(process.env.DATABASE_URL as string);
+const db = pgp(cn);
 
 export default db;
